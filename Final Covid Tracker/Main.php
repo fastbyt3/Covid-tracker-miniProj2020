3c5b2d7be4552ad07fc0921463d3631a33c8ba21
SuDo-Ajay <62496681+SuDo-Ajay@users.noreply.github.com>
2021-05-15 19:37:22 +0530

    Add files via upload

---
+++ Final Covid Tracker/Main.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Covid Tracker, Wear your mask, get vaccinated, Do your part !!, Covid Tracker, Covid case count of India, Covid case count of Tamil Nadu">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Main</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Main.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.14.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "",
+		"@type": "Organization",
+		"name": "",
+		"url": "Main.php",
+		"logo": "images/rz-icons-minimal-lines-black-virus-17-512.png"
+}</script>
+    <meta property="og:title" content="Main">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#8da3c8">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body"><header class="u-align-center-xs u-clearfix u-custom-color-1 u-header u-header" id="sec-11a0"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="#" class="u-align-left u-image u-logo u-image-1" data-image-width="512" data-image-height="512">
+          <img src="images/rz-icons-minimal-lines-black-virus-17-512.png" class="u-logo-image u-logo-image-1" data-image-width="64">
+        </a>
+        <nav class="u-align-left u-menu u-menu-dropdown u-nav-spacing-25 u-offcanvas u-menu-1" data-responsive-from="XL">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700; text-transform: uppercase;">
+            <a class="u-button-style u-custom-active-border-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link" href="#" style="font-size: calc(1em + 2px);">
+              <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 302 302" style="undefined"><use xlink:href="#svg-7b92"></use></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-7b92" x="0px" y="0px" viewBox="0 0 302 302" style="enable-background:new 0 0 302 302;" xml:space="preserve" class="u-svg-content"><g><rect y="36" width="302" height="30"></rect><rect y="236" width="302" height="30"></rect><rect y="136" width="302" height="30"></rect>
+</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-custom-font u-nav u-spacing-30 u-text-font u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-border-2 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-grey-90" href="home.html" style="padding: 10px 0px;">home</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Main.php" style="padding: 8px 0px;">Home</a><a class="u-button-style u-nav-link" href="globaldata.php" style="padding: 8px 0px;">Global Data</a><a class="u-button-style u-nav-link" href="countrydata.php" style="padding: 8px 0px;">Country Data</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <h1 class="u-text u-text-1">CovidTracker</h1>
+      </div></header>
+    <section class="u-align-center u-clearfix u-custom-color-1 u-section-1" id="sec-add3">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-text-1">Wear your mask, get vaccinated, Do your part !!</h1>
+      </div>
+    </section>
+    <section class="u-clearfix u-custom-color-1 u-section-2" id="carousel_4ad6">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-palette-1-base u-shape u-shape-circle u-shape-1"></div>
+        <div class="u-clearfix u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-27 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h1 class="u-custom-font u-text u-text-font u-text-palette-1-dark-1 u-text-1">Covid Tracker<br>
+                  </h1>
+                  <p class="u-text u-text-2">This pandemic has tested everyone out there and have made us go through a lot we never thought we would. The pandemic has turned our lives upside down. Together we can win this !</p>
+                  <a href="/Main.html#sec-0b67" data-page-id="389126" class="u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-btn-1">Check case counts</a>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-left-cell u-size-33 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <div class="u-shape u-shape-svg u-text-palette-3-light-2 u-shape-2">
+                    <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xlink:href="#svg-d73d"></use></svg>
+                    <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-d73d" style="enable-background:new 0 0 160 160;"><path d="M80,30c27.6,0,50,22.4,50,50s-22.4,50-50,50s-50-22.4-50-50S52.4,30,80,30 M80,0C35.8,0,0,35.8,0,80s35.8,80,80,80
+	s80-35.8,80-80S124.2,0,80,0L80,0z"></path></svg>
+                  </div>
+                  <div class="u-border-16 u-border-palette-1-base u-image u-image-circle u-image-1" data-image-width="749" data-image-height="802"></div>
+                  <div class="u-palette-1-base u-preserve-proportions u-shape u-shape-circle u-shape-3"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-custom-color-1 u-section-3" id="sec-0b67">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-align-center u-text u-text-1">Global Covid Case Count</h2>
+        <div class="u-expanded-width u-table u-table-responsive u-table-1">
+          <table class="u-table-entity u-table-entity-1">
+            <colgroup>
+              <col width="25%">
+              <col width="25%">
+              <col width="25%">
+              <col width="25%">
+            </colgroup>
+            <thead class="u-black u-table-header u-table-header-1">
+              <tr style="height: 103px;">
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-1">Active Cases</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-2">Confirmed Cases</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-3">Deceased</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-4">Recovered</th>
+              </tr>
+            </thead>
+            <tbody class="u-table-body">
+            
+            
+            
+            
+            <?php
+
+
+            $db_host        = '127.0.0.1';
+            $db_user        = 'root';
+            $db_pass        = 'JO3hasligm@69';
+            $db_database    = 'miniproj';
+            $db_port        = '3306';
+			
+            global $mysqli;
+            $mysqli = new mysqli(
+              $db_host,
+              $db_user,
+              $db_pass,
+              $db_database,
+              $db_port
+            );
+        
+            if (mysqli_connect_errno()) {
+              printf("Connection to mysql failed miserably :(\n%s", mysqli_connect_error());
+            }
+        
+            $query = "SELECT * FROM global_data";
+            global $res;
+            if (!$res = $mysqli->query($query)) {
+              echo "Invalid query!!\n";
+            }
+        
+            while ($row = mysqli_fetch_array($res)) {
+              echo "<tr style=\"height: 189px;\"><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["active"] . "</td>"
+                . "<td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["confirmed"] . "</td><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["deaths"] . "</td><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["recovered"]
+                . "</td></tr>";
+            }
+            echo "</table>";
+        
+            $mysqli->close();
+        
+            ?>
+            </tbody>
+
+
+
+            
+          </table>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-custom-color-1 u-section-4" id="sec-0a6c">
+      <div class="u-clearfix u-sheet u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-border-13 u-border-palette-1-base u-image u-image-circle u-image-1" data-image-width="803" data-image-height="317"></div>
+        <div class="u-image u-image-circle u-image-2" data-image-width="1600" data-image-height="900"></div>
+        <div class="u-border-20 u-border-palette-1-base u-image u-image-circle u-image-3" data-image-width="1080" data-image-height="1366"></div>
+        <div class="u-shape u-shape-svg u-text-palette-1-base u-shape-1">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xlink:href="#svg-1186"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-1186" style="enable-background:new 0 0 160 160;"><path d="M80,30c27.6,0,50,22.4,50,50s-22.4,50-50,50s-50-22.4-50-50S52.4,30,80,30 M80,0C35.8,0,0,35.8,0,80s35.8,80,80,80
+	s80-35.8,80-80S124.2,0,80,0L80,0z"></path></svg>
+        </div>
+        <div class="u-shape u-shape-svg u-text-palette-3-light-2 u-shape-2">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xlink:href="#svg-525f"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-525f" style="enable-background:new 0 0 160 160;"><path d="M80,30c27.6,0,50,22.4,50,50s-22.4,50-50,50s-50-22.4-50-50S52.4,30,80,30 M80,0C35.8,0,0,35.8,0,80s35.8,80,80,80
+	s80-35.8,80-80S124.2,0,80,0L80,0z"></path></svg>
+        </div>
+        <h4 class="u-text u-text-1">Cases as of May 12 2021</h4>
+        <div class="u-container-style u-expanded-width u-group u-shape-rectangle u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <h1 class="u-align-center u-text u-text-palette-1-dark-1 u-text-2">Country Wise Covid Case Count</h1>
+            <a href="/globaldata.php" data-page-id="389227" class="u-align-center u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-text-active-black u-btn-1">Country data</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-custom-color-1 u-section-5" id="carousel_90aa">
+      <div class="u-clearfix u-sheet u-valign-middle-sm u-sheet-1">
+        <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-32 u-layout-cell-1">
+                    <div class="u-container-layout u-container-layout-1">
+                      <h1 class="u-text u-text-1">Covid case count of India</h1>
+                    </div>
+                  </div>
+                  <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-28 u-layout-cell-2">
+                    <div class="u-container-layout u-container-layout-2">
+                      <p class="u-text u-text-2"> Brief information about state wise case count<br>
+                      </p>
+                      <a href="/countrydata.php" data-page-id="389120" class="u-border-2 u-border-white u-btn u-button-style u-hover-black u-none u-text-body-alt-color u-text-hover-white u-btn-1">state data</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-right-cell u-size-60 u-image-1" data-image-width="1600" data-image-height="828">
+                    <div class="u-container-layout u-container-layout-3"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3 class="u-text u-text-3">Case count of Tamil Nadu as of 12 May 2021</h3>
+      </div>
+    </section>
+    
+    
+    <footer class="u-clearfix u-custom-color-1 u-footer" id="sec-dab2"><div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-center u-container-style u-group u-group-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <p class="u-small-text u-text u-text-variant u-text-1">Site created by:<br>Abishek Ajay Aldeesh Jacob<br>©just_a_small_project. All rights reserved.<br>
+            </p>
+          </div>
+        </div>
+      </div></footer>
+  </body>
+</html>
---
+++ Final Covid Tracker/countrydata.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Our Center, Understanding group therapy, Licensed Professional Counselors Available Anytime, Online Therapy Programs, Family therapy, What is psychotherapy?, Therapy for how w​e live today">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>countrydata</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="countrydata.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.14.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html",
+		"logo": "images/rz-icons-minimal-lines-black-virus-17-512.png"
+}</script>
+    <meta property="og:title" content="countrydata">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#8da3c8">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body"><header class="u-align-center-xs u-clearfix u-custom-color-1 u-header u-header" id="sec-11a0"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="#" class="u-align-left u-image u-logo u-image-1" data-image-width="512" data-image-height="512">
+          <img src="images/rz-icons-minimal-lines-black-virus-17-512.png" class="u-logo-image u-logo-image-1" data-image-width="64">
+        </a>
+        <nav class="u-align-left u-menu u-menu-dropdown u-nav-spacing-25 u-offcanvas u-menu-1" data-responsive-from="XL">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700; text-transform: uppercase;">
+            <a class="u-button-style u-custom-active-border-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link" href="#" style="font-size: calc(1em + 2px);">
+              <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 302 302" style="undefined"><use xlink:href="#svg-7b92"></use></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-7b92" x="0px" y="0px" viewBox="0 0 302 302" style="enable-background:new 0 0 302 302;" xml:space="preserve" class="u-svg-content"><g><rect y="36" width="302" height="30"></rect><rect y="236" width="302" height="30"></rect><rect y="136" width="302" height="30"></rect>
+</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-custom-font u-nav u-spacing-30 u-text-font u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-border-2 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-grey-90" href="home.html" style="padding: 10px 0px;">home</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Main.php" style="padding: 8px 0px;">Home</a><a class="u-button-style u-nav-link" href="globaldata.php" style="padding: 8px 0px;">Global Data</a><a class="u-button-style u-nav-link" href="countrydata.php" style="padding: 8px 0px;">Country Data</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <h1 class="u-text u-text-1">Covid Tracker</h1>
+      </div></header>
+    <section class="u-align-center u-clearfix u-custom-color-1 u-section-1" id="sec-ecea">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-1">State wise case count</h2>
+        <div class="u-expanded-width u-table u-table-responsive u-table-1">
+          <table class="u-table-entity u-table-entity-1">
+            <colgroup>
+              <col width="25%">
+              <col width="25%">
+              <col width="25%">
+              <col width="25%">
+            </colgroup>
+            <thead class="u-black u-table-header u-table-header-1">
+              <tr style="height: 21px;">
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-1">State Name</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-2">Total Cases</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-3">Recovered</th>
+                <th class="u-border-1 u-border-black u-palette-1-base u-table-cell u-table-cell-4">Deceased</th>
+              </tr>
+            </thead>
+            <tbody class="u-table-body">
+
+
+              <?php
+
+
+              $db_host        = '127.0.0.1';
+              $db_user        = 'root';
+              $db_pass        = 'JO3hasligm@69';
+              $db_database    = 'miniproj';
+              $db_port        = '3306';
+        
+              global $mysqli;
+              $mysqli = new mysqli(
+                $db_host,
+                $db_user,
+                $db_pass,
+                $db_database,
+                $db_port
+              );
+          
+              if (mysqli_connect_errno()) {
+                printf("Connection to mysql failed miserably :(\n%s", mysqli_connect_error());
+              }
+              $query = "SELECT * FROM Regional_data";
+              global $res;
+              if (!$res = $mysqli->query($query)) {
+                echo "Invalid query!!\n";
+              }
+          
+              while ($row = mysqli_fetch_array($res)) {
+                echo "<tr style=\"height: 75px;\"><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["statename"] . "</td>"
+                  . "<td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["totalCases"] . "</td><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["recoveries"] . "</td><td class=\"u-border-1 u-border-grey-30 u-table-cell\">" . $row["deaths"]
+                  . "</td></tr>";
+              }
+              echo "</table>";
+          
+              $mysqli->close();
+          
+              ?>
+
+
+
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-right u-clearfix u-custom-color-1 u-section-2" id="sec-20de">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-custom-color-1 u-expanded-height u-shape u-shape-rectangle u-shape-1"></div>
+        <div class="u-clearfix u-expanded-width u-gutter-20 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <h3 class="u-custom-font u-font-montserrat u-text u-text-palette-1-base u-text-1">Covid case count of all the countries</h3>
+                  <a href="/globaldata.php" data-page-id="389227" class="u-active-palette-1-dark-2 u-align-center u-border-2 u-border-active-palette-1-dark-2 u-border-custom-color-1 u-border-hover-palette-1-dark-2 u-btn u-btn-round u-button-style u-hover-custom-color-1 u-palette-1-base u-radius-50 u-text-active-white u-text-black u-text-hover-white u-btn-1">Global count</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-clearfix u-custom-color-1 u-footer" id="sec-dab2"><div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-center u-container-style u-group u-group-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <p class="u-small-text u-text u-text-variant u-text-1">Site created by:<br>Abishek Ajay Aldeesh Jacob<br>©just_a_small_project. All rights reserved.<br>
+            </p>
+          </div>
+        </div>
+      </div></footer>
+  </body>
+</html>
